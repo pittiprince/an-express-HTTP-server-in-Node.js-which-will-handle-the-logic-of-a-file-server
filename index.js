caa7cb4c403ace9c86a5c23dc22652b85294a1d6
pittiprince <117570887+pittiprince@users.noreply.github.com>
2024-02-03 17:08:58 +0000

    done

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 
+const express = require('express');
+const app=express();
+const fs = require('fs'); 
+const path = require('path');
+
+
+app.get("/files",(req,res)=>{
+fs.readdir(__dirname,(err,files)=>{
+  if(err){
+    console.log("error");
+  }
+  res.json({
+    files
+  })
+})
+}
+)  
+
+app.get("/files/:filename",(req,res)=>{
+  fs.readFile(req.params.filename,'utf8',(err,data)=>{
+    if(err){
+      console.log("error!");
+    }
+    let output =JSON.stringify(data);
+    res.json({
+     output
+    })
+  })
+})
+
+app.listen(3000);
+
+
